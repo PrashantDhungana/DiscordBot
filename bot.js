0cb53add599c24d5ba62f01af620a179667a0824
Prashant Dhungana <prashantsharma.dhungana@texasintl.edu.np>
2020-11-30 15:25:16 +0545

    Added the meme feature

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 console.log("Hey! This is PrashantBot!");
 require("dotenv").config();
 
+const ytdl = require("ytdl-core");
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fetch = require("node-fetch");
@@ -11,10 +13,12 @@ const PREFIX = "$";
 client.on("ready", () => console.log("Connected"));
 
 client.on("message", async (message) => {
+  //
+
   if (message.author.bot) return;
 
   const msg = message.content;
-  const regex = /\b[^-!0-9\n]+\b/;
+  const regex = /^[^-!0-9\n\s]*$/;
 
   //Among Us Code sent then send Upu is the imposter !
   if (msg.length === 6 && msg.match(regex)) {
@@ -101,6 +105,79 @@ client.on("message", async (message) => {
       }
       request(options, callback);
     }
+    //Send list of commands for the bot
+    else if (CMD_NAME === "commands" || CMD_NAME === "command") {
+      const embed = new Discord.MessageEmbed()
+        .setColor("#058ba7")
+        .setTitle("List of Commands")
+        .addFields(
+          { name: "$define [word]", value: "Defines the given [word] " },
+          { name: "$joke", value: "Sends a random joke" },
+          {
+            name: "$meme [opt:dank (d),wholesome(w)]",
+            value:
+              "dank or d : sends a dank meme \n wholesome or w : sends a wholesome meme",
+          }
+        );
+
+      message.channel.send(embed);
+    }
+
+    //Send a dank or a wholesome meme
+    else if (CMD_NAME === "meme" || CMD_NAME === "memes") {
+      if (!args.length) {
+        memeGrabber();
+      } else if (args[0] === "dank" || args[0] === "d") {
+        //send dank meme
+        memeGrabber("lmGoingToHellForThis");
+      } else if (args[0] === "wholesome" || args[0] === "w") {
+        //send a wholesome meme
+        memeGrabber("wholesomememes");
+      } else {
+        message.channel.send(
+          "Invalid meme type!!! \nCheck $command for the list of arguments"
+        );
+      }
+    }
+  }
+  function memeGrabber(type = null) {
+    //For a simple meme
+    if (type == null) {
+      type = "memes";
+    }
+    //Grab from the Reddit API
+
+    const options = {
+      url: "https://api.reddit.com/r/" + type + "/hot",
+      headers: {
+        "User-Agent": "my bot 1.0",
+      },
+    };
+
+    //Request the API
+    function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const info = JSON.parse(body);
+        const newdata = info.data.children;
+
+        //Generate random jokes each time
+        r = Math.floor(Math.random() * newdata.length);
+        const title = newdata[r]["data"]["title"];
+
+        //FIX IF THIS VALUE WILL BE NULL
+        do {
+          url = newdata[r]["data"]["url_overridden_by_dest"];
+        } while (url == null);
+        message.channel.send(title, {
+          files: [url],
+        });
+      } else {
+        message.channel.send("An Error occured. Please Try Again later!");
+      }
+    }
+    request(options, callback);
+
+    // message.channel.send(type + "meme invoked");
   }
 });
 client.login(process.env.API_KEY);
